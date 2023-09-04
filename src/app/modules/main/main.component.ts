8ac7964a32b1f10ea425ec67b05428cf13d7ed07
alika <alika.shako@gmail.com>
2023-09-04 22:20:27 +0400

    fix

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -7,74 +7,10 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-  // @ts-ignore
-
-  chosenIndex: number = -1;
-  defChosen: boolean = true;
-  ngOnInit(): void {
+  
 
   }
-  slidesStore: any = [
-    {
-      id: 1,
-      src: '/assets/img/пе-e1689937028637-fotor-20230728161841.png',
-    },
-    {
-      id: 2,
-      src: '/assets/img/277291315_509339724087745_6772757857673628723_n.jpg',
-    },
-    {
-      id: 3,
-      src: '/assets/img/357107358_280082017862749_175586619828625337_n.jpg',
-    },
-
-  ];
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: true,
-    navSpeed: 700,
-    navText: ['<i class="fa fa-arrow-left" aria-hidden="true"></i>', '<i class="fa fa-arrow-right" aria-hidden="true"></i>'],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 1
-      },
-      740: {
-        items: 1
-      },
-      940: {
-        items: 1
-      }
-    },
-    nav: true
-  }
-
-  slidesStoreSecond: any = [
-    {
-      id: 1,
-      src: '/assets/img/music img.jpg',
-    },
-    {
-      id: 2,
-      src: '/assets/img/music img 2.jpg',
-    },
-    {
-      id: 3,
-      src: '/assets/img/music img 3.jpg',
-    },
-
-  ];
 
 
-  choseTitle(index: any) {
-    this.chosenIndex = index;
-    this.defChosen = false;
-  }
-
-}
+ 
  
